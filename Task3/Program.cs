7bba67e0a5bd0242f9c54c63409006cc7c033df8
Alice <zhupanova2017sv@mail.ru>
2023-02-24 19:25:16 +0300

    Arithmetic Mean

---
+++ Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+System.Console.WriteLine();
+PrintArray(matrix);
+
+// Console.WriteLine(matrix.GetLength(0));
+System.Console.WriteLine();
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    double ArithmeticMean =  sum / matrix.GetLength(0);
+    System.Console.WriteLine($"Среднее арифметическое каждого столбца {ArithmeticMean}");
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
